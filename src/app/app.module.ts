99e4e745cdc6df1b2732a3b7f375ee6cdd5e8c75
Piyumi Prasandika <10749850@students.plymouth.ac.uk>
2023-01-24 00:48:58 +0530

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsListComponent } from './components/dashboard/products-list/products-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+import { OrderComponent } from './components/order/order.component';
+import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
+import { ProductListAdminComponent } from './components/product/product-list-admin/product-list-admin.component';
+import { ProductItemComponent } from './components/dashboard/product-item/product-item.component';
+import { CartComponent } from './components/cart/cart.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ProductsListComponent,
+    NavBarComponent,
+    OrderComponent,
+    ProductDetailsComponent,
+    ProductListAdminComponent,
+    ProductItemComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
