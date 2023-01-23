37b610872e20b84b56cd2295ed248d7aa8dbf244
Piyumi Prasandika <10749850@students.plymouth.ac.uk>
2023-01-24 00:24:10 +0530

    Update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: 'https://pharmacy-be-production.up.railway.app',
+  firebaseConfig: {
+    apiKey: 'AIzaSyD0ChSnHgiM9tVmBd7G-yFK5M9yxOLSBTc',
+    authDomain: 'api-sd-md.firebaseapp.com',
+    projectId: 'api-sd-md',
+    storageBucket: 'api-sd-md.appspot.com',
+    messagingSenderId: '30921033948',
+    appId: '1:30921033948:web:4b80098e5a90ef8afc4c19',
+    measurementId: 'G-4X1CV8JFMN',
+  },
 };
 
 /*
