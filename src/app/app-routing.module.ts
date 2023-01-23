eb4993dc0d38f29f38e243e53c21163da2b56fd4
Piyumi Prasandika <10749850@students.plymouth.ac.uk>
2023-01-24 00:26:20 +0530

    modify routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,74 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductItemComponent } from './components/dashboard/product-item/product-item.component';
+import { OrderComponent } from './components/order/order.component';
+import { ProductComponent } from './components/product/product.component';
+import { UserRole } from './enums/user-role.enum';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { RoleGuard } from './guards/role/role.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: [UserRole.Admin],
+    },
+  },
+  {
+    path: 'dashboard/products/:productId',
+    component: ProductItemComponent,
+    canActivate: [AuthGuard],
+    data: {
+      roles: [UserRole.Admin],
+    },
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: [UserRole.Regular],
+    },
+  },
+  {
+    path: 'orders',
+    component: OrderComponent,
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard, RoleGuard],
+    component: DashboardComponent,
+    data: {
+      roles: [UserRole.Regular],
+    },
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: '*',
+    redirectTo: '/auth/login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
